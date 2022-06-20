77567a6449e5ae3fdcfffc04e87689d2a6e45b6f
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-06-20 09:37:00 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿int num = 0;
+int result = 0;
+string input;
+Stack stack = new Stack(100);
+
+Console.Write("Digite:");
+while ((input = Console.ReadLine() ?? "0") != "exit")
+{
+    int n;
+    bool isNumeric = int.TryParse(input, out n);
+    if (isNumeric)
+    {
+        if (stack.IsFull())
+            Console.WriteLine("StackOverFlow");
+        else
+            stack.Push(int.Parse(input)); ;
+    }
+    else
+    {
+        num = stack.Pop();
+        if (stack.IsEmpty())
+        {
+            Console.WriteLine("É nescessario pelo menos 2 numeros para fazer uma operação");
+        }
+        else
+        {
+            switch (input)
+            {
+                case "-":
+                    result = stack.Pop() - num;
+                    break;
+                case "+":
+                    result = stack.Pop() + num;
+                    break;
+                case "*":
+                    result = stack.Pop() * num;
+                    break;
+                case "/":
+                    result = stack.Pop() / num;
+                    break;
+            }
+            stack.Push(result);
+            Console.WriteLine(result);
+        }
+
+    }
+    Console.Write("Digite:");
+}
---
+++ Stack.cs
@@ -0,0 +1,33 @@
+﻿class Stack
+{
+    private int[] vet;
+    private int top;
+
+    public Stack(int tam)
+    {
+        vet = new int[tam];
+        top = 0;
+    }
+
+    public void Push(int x)
+    {
+        vet[top] = x;
+        top++;
+    }
+
+    public int Pop()
+    {
+        top--;
+        return (vet[top]);
+    }
+
+    public bool IsEmpty()
+    {
+        return (top == 0);
+    }
+
+    public bool IsFull()
+    {
+        return (top == vet.Length);
+    }
+}
